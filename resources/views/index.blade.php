bcb465eacdc86261b158ed38b61aff8d59000037
infinity-dream-code <infinity-dream-code@gmail.com>
2025-10-17 10:53:09 +0700

    p

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -420,7 +420,7 @@
     document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("academic_year");
 
-  fetch("http://103.23.103.43/WEB_TAGIHAN_PROJECT/WS_DEMO_MULTIPLE/index.php?path=list-tahun-aka")
+  fetch("http://10.99.23.111/WEB_TAGIHAN_PROJECT/WS_DEMO_MULTIPLE/index.php?path=list-tahun-aka")
         .then(response => response.json())
         .then(data => {
             select.innerHTML = ""; 
